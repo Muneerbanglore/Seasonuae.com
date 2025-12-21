8cb2df3107a0e13887c518b02f71c22146432a17
Muneerbanglore <muneerbnglur@gmail.com>
2025-12-22 02:50:35 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Seasonuae.com/',   // ⚠️ repo name exactly
+  base: '/',   // ⚠️ repo name exactly
 })
 
